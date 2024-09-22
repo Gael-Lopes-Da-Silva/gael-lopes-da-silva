81e66729350ef0d5738d3e15941ea9236f849a3e
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-22 16:47:33 +0200

    Update website

--- build.py
+++ build.py
@@ -34,9 +34,9 @@ def generateMarkdown():
         file.write('~~~\n')
         file.write('\n')
         file.write('**[<kbd> <br> Website <br> </kbd>][Website]**\n')
-        file.write('<img align="right" style="width:37px;" title="This is the yellow dancing man. Don\'t question him." alt="Too bad. He gone..." src="./img/yellow_man.gif">\n')
+        file.write('<img align="right" style="width:37px;" title="This is the yellow dancing man. Don\'t question him." alt="Too bad. He gone..." src="./assets/yellow_man.gif">\n')
         file.write('\n')
-        file.write('[Website]: https://gael-lopes-da-silva.ovh/\n')
+        file.write('[Portfolio]: https://gael-lopes-da-silva.github.io/portfolio/\n')
 
 if __name__ == "__main__":
     generateMarkdown()
